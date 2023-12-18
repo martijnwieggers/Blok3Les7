114a30d50768e3a73dca4a8cf76cf31bd3f9137b
Martijn Wieggers <127099600+martijnwieggers@users.noreply.github.com>
2023-12-18 12:59:27 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Blok3Les7;
+
+List<Vliegtuig> Vliegtuigen = new List<Vliegtuig>();
+
+//Stap 7 :Maak 3 auto's aan en gebruik de constructor zodat direct die eigenschappen zijn gevuld.
+//Voeg deze auto's toe aan de lijst met auto's  Autos.Add()
+
+
+//Stap 8: Voeg ook 1 auto toe waarbij je de een nieuwe auto aanmaakt
+//en vervolgens de methode VraagInformatie() van de Auto class aanroept.
+
+foreach (Vliegtuig vliegtuig in Vliegtuigen)
+{
+    //Stap 9: Druk van iedere vliegtuig de informatie af door gebruik te maken van de ToonInformatie() methode van ieder vliegtuig.
+
+}
---
+++ Vliegtuig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blok3Les7
+{
+    public class Vliegtuig
+    {
+        //Stap 3: Maak hier de publieke eigenschappen aan.
+
+        //Stap 4: Maak hier de constructor aan waarin direct alle eigenschappen (Merk, Type, AantalMotoren, MiximumSnelheid en Aantal Passagiers) worden meegegeven.
+
+        //Stap 5: Maak de Methode ToonInfo() aan die alle informatie afdrukt.
+
+        //Stap 6: Maak de Methode VraagInfo() deze stelt de vragen
+        //        en slaat deze op in de eigenschappen van de class.
+
+        //Default Constructor: Afblijven en laten staan !
+        public Vliegtuig()
+        {
+        }
+
+
+    }
+}
