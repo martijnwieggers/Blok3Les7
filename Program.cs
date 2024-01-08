4c2d3a6f9e9017c92c6be990ce6f7fc4ad537980
Martijn Wieggers <127099600+martijnwieggers@users.noreply.github.com>
2024-01-08 10:57:54 +0100

    Auto vervangen door Vliegtuig in comments

--- Program.cs
+++ Program.cs
@@ -2,12 +2,12 @@
 
 List<Vliegtuig> Vliegtuigen = new List<Vliegtuig>();
 
-//Stap 7 :Maak 3 auto's aan en gebruik de constructor zodat direct die eigenschappen zijn gevuld.
-//Voeg deze auto's toe aan de lijst met vliegtuigen  Vliegtuigen.Add(....)
+//Stap 7 :Maak 3 vliegtuigen aan en gebruik de constructor zodat direct die eigenschappen zijn gevuld.
+//Voeg deze vliegtuigen toe aan de lijst met vliegtuigen  Vliegtuigen.Add(....)
 
 
-//Stap 8: Voeg ook 1 auto toe waarbij je een nieuwe auto aanmaakt
-//en vervolgens de methode VraagInformatie() van de het aangemaakte object van de Auto class aanroept.
+//Stap 8: Voeg ook 1 vliegtuig toe waarbij je een nieuw vliegtuig aanmaakt
+//en vervolgens de methode VraagInformatie() van de het aangemaakte object van de vliegtuig class aanroept.
 
 foreach (Vliegtuig vliegtuig in Vliegtuigen)
 {
